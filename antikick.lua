72fd78a796663e03a7f9889574f9f4c912e411ea
Tobi <68130707+tobi437a@users.noreply.github.com>
2024-03-30 12:44:47 +0100

    Create antikick.lua

---
+++ antikick.lua
@@ -0,0 +1,18 @@
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
+
+local originalNamecall = mt.__namecall
+
+mt.__namecall = newcclosure(function(self, ...)
+    local methodName = getnamecallmethod()
+    local args = {...}
+
+    if methodName == "Kick" and self == game.Players.LocalPlayer then
+        warn("Attempt to kick LocalPlayer prevented.")
+        return
+    end
+
+    return originalNamecall(self, unpack(args))
+end)
+
+setreadonly(mt, true)
